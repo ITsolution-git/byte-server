360fca494f22c16571b716efa0580b68eedc1a32
Irfan Fadilah <irfan@41studio.com>
2016-09-26 14:30:26 +0700

    Change Email Service to Amazon SES

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,11 @@
 require File.expand_path('../application', __FILE__)
 
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.mandrillapp.com',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => Figaro.env.mandrill_username,
-    :password       => Figaro.env.mandrill_password,
+    :address        => 'email-smtp.us-east-1.amazonaws.com',
+    :port           => 587,
+    :authentication => :login,
+    :user_name      => Figaro.env.ses_username,
+    :password       => Figaro.env.ses_password,
     :domain         => 'mybyteapp.com',
     :enable_starttls_auto => true
   }
