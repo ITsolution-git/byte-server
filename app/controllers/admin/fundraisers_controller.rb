87d4c4e0991dacab19fcc05983f5d708283ea473
roy.smith0820 <roy.smith0820@gmail.com>
2016-12-21 05:04:36 -0500

    deletebutton,type feature, validation

--- app/controllers/admin/fundraisers_controller.rb
+++ app/controllers/admin/fundraisers_controller.rb
@@ -23,8 +23,16 @@ class Admin::FundraisersController < ApplicationController
   # GET /fundraisers/new
   # GET /fundraisers/new.json
   def new
+    type_array = Fundraiser.select(:division_type).uniq
+    type_array.uniq(true);
+    @types = [];
+    type_array.each do |type|
+      @types<<type[:division_type]
+    end 
     @fundraiser = Fundraiser.new
     @fundraiser.status = 2;
+    @type_array = Fundraiser.select(:division_type).uniq
+    @type_array.uniq(true);
     respond_to do |format|
       format.html # new.html.erb
     end
@@ -32,17 +40,34 @@ class Admin::FundraisersController < ApplicationController
 
   # GET /fundraisers/1/edit
   def edit
+    type_array = Fundraiser.select(:division_type).uniq
+    type_array.uniq(true);
+    @types = [];
+    type_array.each do |type|
+      @types<<type[:division_type]
+    end
     @fundraiser = Fundraiser.find(params[:id])
   end
 
   # POST /fundraisers
   # POST /fundraisers.json
   def create
+    type_array = Fundraiser.select(:division_type).uniq
+    type_array.uniq(true);
+    @types = [];
+    type_array.each do |type|
+      @types<<type[:division_type]
+    end
     @fundraiser = Fundraiser.new(params[:fundraiser])
     if params[:logo_public_id].present?
       preloaded = Cloudinary::PreloadedFile.new(params[:logo_public_id])         
       raise "Invalid upload signature" if !preloaded.valid?
       @fundraiser.logo = preloaded.identifier
+    end
+    if params[:division_public_id].present?
+      preloaded = Cloudinary::PreloadedFile.new(params[:division_public_id])         
+      raise "Invalid upload signature" if !preloaded.valid?
+      @fundraiser.division_image = preloaded.identifier
     end
     respond_to do |format|
       if @fundraiser.save
@@ -57,18 +82,31 @@ class Admin::FundraisersController < ApplicationController
   # PUT /fundraisers/1.json
   def update
     @fundraiser = Fundraiser.find(params[:id])
-    puts @fundraiser
     if @fundraiser.logo != params[:logo_public_id] and params[:logo_public_id].present?
       preloaded = Cloudinary::PreloadedFile.new(params[:logo_public_id])         
       raise "Invalid upload signature" if !preloaded.valid?
       @fundraiser.logo = preloaded.identifier
 
     end
+    if @fundraiser.division_image != params[:division_public_id] and params[:division_public_id].present?
+      preloaded = Cloudinary::PreloadedFile.new(params[:division_public_id])         
+      raise "Invalid upload signature" if !preloaded.valid?
+      @fundraiser.division_image = preloaded.identifier
+    end
+    type_array = Fundraiser.select(:division_type).uniq
+    type_array.uniq(true);
+    @types = [];
+    type_array.each do |type|
+      @types<<type[:division_type]
+    end
+    puts params[:fundraiser]
+    puts "$"*100
+    puts @fundraiser.credit_card_type
     respond_to do |format|
       if @fundraiser.update_attributes(params[:fundraiser])
         format.html { redirect_to admin_fundraisers_url, notice: 'Fundraiser was successfully updated.' }
       else
-        format.html { render action: "new" }
+        format.html { render action: "edit" }
       end
     end
   end
@@ -80,8 +118,7 @@ class Admin::FundraisersController < ApplicationController
     @fundraiser.destroy
 
     respond_to do |format|
-      format.html { redirect_to fundraisers_url }
-      format.json { head :no_content }
+      format.html { redirect_to admin_fundraisers_url }
     end
   end
 end
--- app/models/fundraiser.rb
+++ app/models/fundraiser.rb
@@ -2,4 +2,36 @@ class Fundraiser < ActiveRecord::Base
   attr_accessible :address, :alt_email, :city, :credit_card_expiration_date, :credit_card_number, :credit_card_security_code, :credit_card_type, :division_image, :division_type, :email, :fundraiser_name, :name, :phone, :state, :status, :url, :url, :zipcode, :locations
 
   has_and_belongs_to_many :locations
+
+  validates_format_of :url, :with => /^[w]{3}\.[\S]+\.[\S]+/, :allow_blank=> true
+  validates :phone,:allow_blank=> true,
+    :format => { :with => /^\(([0-9]{3})\)([ ])([0-9]{3})([-])([0-9]{4})$/,
+      :message => "^Invalid Phone number format. Use: (xxx) xxx-xxxx"}
+
+  validates :email, presence: { message: "can't be blank" }
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  # validates_format_of :alt_email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :name, :presence => {message:"^Fundraiser name can't be blank"},
+    length:{ maximum: 40,:message => "^Fundraiser Name can't be greater than 40 characters."}
+  validates :fundraiser_name, :presence => true  
+
+  validates :address, :presence => true
+  validates :city, :presence => true
+  validates :state, :presence => true
+  validates :zipcode,
+            presence: { message:"can't be blank" },
+            numericality: { message: "is not a number" } ,
+            length: { is: 5 }
+
+  validates :credit_card_type, presence: true
+  validates :credit_card_number, presence: true, numericality: { message: 'must be numbers' }
+  validates :credit_card_expiration_date,
+            presence: true,
+            format: { with: /^((0[1-9])|(1[0-2]))\/(\d{2})$/,
+                      message: "^Invalid expiration date - please use the format: MM/YY"
+                    }
+  validates :credit_card_security_code,
+            presence: true,
+            numericality: { message: "must be numbers" }
+
 end
